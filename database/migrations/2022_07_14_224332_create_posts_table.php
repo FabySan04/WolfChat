a8f216a8718a4f101d795725c3459b5bfc612bb9
FabySan04 <95632687+FabySan04@users.noreply.github.com>
2022-12-07 16:49:02 -0600

    Add files via upload

---
+++ database/migrations/2022_07_14_224332_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_user');
+            $table->string('name');
+            $table->string('foto_user');
+            $table->string('contenido');
+            $table->string('imagen');
+            $table->string('fecha');
+            $table->timestamps();
+            
+        });
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/migrations/2022_11_08_223236_create_comentarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_post');
+            $table->string('id_user');
+            $table->string('name');            
+            $table->string('foto_user');
+            $table->string('comentario');
+            $table->string('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
